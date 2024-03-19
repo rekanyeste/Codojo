f97ab3c58e4af813ddea46f8be4cbcd396a1cfa7
Nyeste Reka <rekabeka37@gmail.com>
2024-03-19 21:27:22 +0100

    A regisztrációs felület kialakítása a layout-ban

--- app/src/main/java/com/example/nyeste_reka/RegisterActivity.java
+++ app/src/main/java/com/example/nyeste_reka/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.nyeste_reka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
