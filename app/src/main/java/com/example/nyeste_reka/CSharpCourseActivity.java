82538161b00a98352221558d6147926b88f6c44d
Nyeste Reka <rekabeka37@gmail.com>
2024-03-31 22:45:33 +0200

    Kurzusok létrehozása és a navigálógombok implementálása

---
+++ app/src/main/java/com/example/nyeste_reka/CSharpCourseActivity.java
@@ -0,0 +1,14 @@
+package com.example.nyeste_reka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CSharpCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_csharp_course);
+    }
+}
--- app/src/main/java/com/example/nyeste_reka/QuizActivity.java
+++ app/src/main/java/com/example/nyeste_reka/QuizActivity.java
@@ -2,13 +2,36 @@ package com.example.nyeste_reka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
 
+    Button buttonJava, buttonCSharp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        buttonJava = findViewById(R.id.btnCourseJava);
+        buttonJava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, JavaCourseActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        buttonCSharp = findViewById(R.id.btnCourseCSharp);
+        buttonCSharp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, CSharpCourseActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
