49ee1064c9b3f7dac72d51cfb3ee8f6e828cad92
Nyeste Reka <rekabeka37@gmail.com>
2024-04-01 23:25:54 +0200

    Java kurzusok és kvízek listája kész, navigálógombok példányosítva

--- app/src/main/java/com/example/nyeste_reka/JavaCourseListActivity.java
+++ app/src/main/java/com/example/nyeste_reka/JavaCourseListActivity.java
@@ -2,13 +2,37 @@ package com.example.nyeste_reka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JavaCourseListActivity extends AppCompatActivity {
 
+    Button homePage, backToChoseCourse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_course_list);
+
+        homePage = findViewById(R.id.backToHome);
+        backToChoseCourse = findViewById(R.id.backToCourses);
+
+        homePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(JavaCourseListActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        backToChoseCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(JavaCourseListActivity.this, QuizActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
