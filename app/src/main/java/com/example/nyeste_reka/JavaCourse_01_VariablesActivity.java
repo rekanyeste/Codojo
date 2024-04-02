0c85dd7fc81fff381f61ff7243670f322bfc99b0
Nyeste Reka <rekabeka37@gmail.com>
2024-04-02 10:42:10 +0200

    Változók tananyag feltöltve

--- app/src/main/java/com/example/nyeste_reka/JavaCourse_01_VariablesActivity.java
+++ app/src/main/java/com/example/nyeste_reka/JavaCourse_01_VariablesActivity.java
@@ -2,13 +2,36 @@ package com.example.nyeste_reka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JavaCourse_01_VariablesActivity extends AppCompatActivity {
 
+    Button btnBack, btnNextToQuizVariables;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_course01_variables);
+
+        btnBack = findViewById(R.id.btnBackJavaCourseList01);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(JavaCourse_01_VariablesActivity.this, JavaCourseListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnNextToQuizVariables = findViewById(R.id.btnNextCourseJava01Quiz);
+        btnNextToQuizVariables.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(JavaCourse_01_VariablesActivity.this, JavaCourse_02_VariablesQuizActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
