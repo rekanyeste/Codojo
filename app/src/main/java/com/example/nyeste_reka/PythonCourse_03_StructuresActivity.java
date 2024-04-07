b945ba26b29149445a5d5ab976e0066a4d827461
Nyeste Reka <rekabeka37@gmail.com>
2024-04-08 00:30:28 +0200

    Adatszerkezet tananyag és navigálógombok készen vannak

--- app/src/main/java/com/example/nyeste_reka/PythonCourse_03_StructuresActivity.java
+++ app/src/main/java/com/example/nyeste_reka/PythonCourse_03_StructuresActivity.java
@@ -2,13 +2,28 @@ package com.example.nyeste_reka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PythonCourse_03_StructuresActivity extends AppCompatActivity {
-
+    Button btnBack, btnNextStructures;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_python_course03_structures);
+        btnBack = findViewById(R.id.backPythonCourseList03);
+        btnNextStructures = findViewById(R.id.nextCoursePython03);
+
+        btnBack.setOnClickListener(view -> {
+            Intent intent = new Intent(PythonCourse_03_StructuresActivity.this, PythonCourseListActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        btnNextStructures.setOnClickListener(view -> {
+            Intent intent = new Intent(PythonCourse_03_StructuresActivity.this, PythonCourse_04_RegExActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
